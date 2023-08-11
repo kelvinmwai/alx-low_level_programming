585d98bb0faacf38ae0f1e11664471ba53669a31
kelvinmwai <muthikelvin22@gmail.com>
2023-08-11 03:54:08 -0700

    alphABET

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - the alphabet in lowercase, and then in uppercase
+ * Return: 0
+ */
+int main(void)
+{
+	int x = 97;
+	int y = 65;
+
+	while (x <= 122)
+	{
+		putchar(x);
+	}
+	while (y <= 90)
+	{
+		putchar(y);
+	}
+	putchar('\n');
+	return (0);
+}
+
