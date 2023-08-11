0ea11f4eb84d9780c4e39ccb03fe725dc2b82a14
kelvinmwai <muthikelvin22@gmail.com>
2023-08-11 07:21:18 -0700

    Inventing is a combination of brains and materials

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -8,21 +8,18 @@ int main(void)
 {
 	int n, m;
 
-	for (n = 0; n < 9; n++)
+	for (n = 0; n < 10; n++)
 	{
 		for (m = n + 1; m < 10; m++)
 		{
 			putchar((n % 10) + '0');
 			putchar((m % 10) + '0');
+			putchar(',');
+			putchar(' ');
 
 			if (n == 0 && m == 9)
 				continue;
-
-			putchar(',');
-			putchar(' ');
 		}
-		putchar(',');
-		putchar(' ');
 	}
 	putchar('\n');
 	return (0);
