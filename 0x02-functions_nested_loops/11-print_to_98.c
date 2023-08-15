a9a9d9fbef5c3b373a28c71732ee4e55867a39c0
kelvinmwai <muthikelvin22@gmail.com>
2023-08-15 06:14:55 -0700

    98 Battery Street, the OG

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -3,7 +3,6 @@
 /**
  * print_to_98 - Prints all natural numbers from n to 98
  * @n: The number to start printing from
- * Return: 0
  */
 void print_to_98(int n)
 {
@@ -11,7 +10,7 @@ void print_to_98(int n)
 
 	if (n <= 98)
 	{
-		for (i = n ; i < 98; i++)
+		for (i = n ; i <= 98; i++)
 		{
 			if (i != 98)
 				printf("%d, ", i);
