ebcb647321f5dfc85d729f720a1858b5158541a8
kelvinmwai <muthikelvin22@gmail.com>
2023-08-15 08:02:10 -0700

    fibonacci 16

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+/**
+ * main - prints first 98 fibonacci numbers
+ * Return: 0
+ */
+int main(void)
+{
+	unsigned long int i;
+	unsigned long int b = 1;
+	unsigned long int a = 2;
+	unsigned long int l = 10000000000;
+	unsigned long int b1;
+	unsigned long int b2;
+	unsigned long int a1;
+	unsigned long int a2;
+
+	printf("%lu", b);
+
+	for (i = 1; i < 91; i++)
+	{
+		printf(", %lu", a);
+		a += b;
+		b = a - b;
+	}
+
+	b1 = (b / l);
+	b2 = (b % l);
+	a1 = (a / l);
+	a2 = (a % l);
+
+	for (i = 92; i < 99; ++i)
+	{
+		printf(", %lu", a1 + (a2 / l));
+		printf("%lu", a2 % l)
+		a1 = a1 + b1;
+		b1 = a1 - b1;
+		a2 = a2 + b2;
+		b2 = a2 - b2;
+	}
+	printf("\n");
+	return (0);
+}
