adb22daf6cf27dd150ed1419aae820c4bdfe2b3c
kelvinmwai <muthikelvin22@gmail.com>
2023-08-15 00:53:17 -0700

    I sometimes suffer from insomnia

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * print_alphabet - Entry point
+ * Description: 'prints alphabet in lowercase'
+ * Return: 0
+ */
+void print_alphabet(void)
+{
+	char ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+		_putchar(ch);
+	_putchar('\n');
+}
