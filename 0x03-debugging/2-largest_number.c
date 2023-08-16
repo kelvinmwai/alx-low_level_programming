a050993600abd24557593ad945672b96005cefe5
kelvinmwai <muthikelvin22@gmail.com>
2023-08-16 05:32:46 -0700

    0 > 972?

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -16,10 +16,6 @@ int largest_number(int a, int b, int c)
 	{
 	largest = a;
 	}
-	else if (a > b && c < a)
-	{
-	largest = c;
-	}
 	else if (b > c)
 	{
 	largest = b;
@@ -28,5 +24,6 @@ int largest_number(int a, int b, int c)
 	{
 	largest = c;
 	}
+
 	return (largest);
 }
