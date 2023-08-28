58926d5cf2fbcfce082b91522e0f04cd6c761867
kelvinmwai <muthikelvin22@gmail.com>
2023-08-28 05:19:15 -0700

    2. strchr

--- 0x07-pointers_arrays_strings/_putchar.c
+++ 0x07-pointers_arrays_strings/_putchar.c
@@ -1,7 +1,6 @@
 #include <unistd.h>
 
 /**
-<<<<<<< HEAD
  * _putchar - writes the character c to stdout
  * @c: The character to print
  *
@@ -11,27 +10,4 @@
 int _putchar(char c)
 {
 	return (write(1, &c, 1));
-=======
- *  * _putchar - writes the character c to stdout
- *   * @c: The character to print
- *    *
- *     * Return: On success 1.
- *      * On error, -1 is returned, and errno is set appropriately.
- *       */
-int _putchar(char c)
-{
-		return (write(1, &c, 1));
-}#include <unistd.h>
-
-/**
- *  * _putchar - writes the character c to stdout
- *   * @c: The character to print
- *    *
- *     * Return: On success 1.
- *      * On error, -1 is returned, and errno is set appropriately.
- *       */
-int _putchar(char c)
-{
-		return (write(1, &c, 1));
->>>>>>> 51acb1e617652cd850161e31ab3028067e2e30c4
 }
