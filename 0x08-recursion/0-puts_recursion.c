775af37ecf56ea4da1281955a3335ff08c07d3e8
kelvinmwai <muthikelvin22@gmail.com>
2023-08-30 09:19:05 -0700

    task 0

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,8 +1,8 @@
 #include "main.h"
 /**
- * _puts_recurssion - function like puts();
+ * _puts_recursion - function like puts();
  * @s: input
- * Return: Always 0
+ * Return: Always 0 (Success)
  */
 
 void _puts_recursion(char *s)
